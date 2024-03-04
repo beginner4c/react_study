af22dd7ebbd7eef4124f1bb5e1341fcbf217c443
박명규 <parkmyung-kyu@bagmyeong-gyuui-MacBookPro.local>
2024-03-04 10:25:27 +0900

    coin tracker is done

--- coin_tracker/src/App.js
+++ coin_tracker/src/App.js
@@ -2,16 +2,35 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]); // 빈 array라도 두는 이유는 없는 경우 undefine으로 정의되기 때문
   useEffect (() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
-    .then((response => response.json ))
-    .then((json) => console.log(json));
+    .then((response) => response.json())
+    .then((json) => {
+      setCoins(json);
+      setLoading(false);
+    });
   }, []);
   
   return (
     <div>
-      <h1>The Coins</h1>
-      {loading ? <strong>Loading...</strong> : null}
+      <h1>The Coins {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? <strong>Loading...</strong> : <select>
+        {coins.map((coin) => (
+          <option>
+            {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+          </option>
+        ))}
+      </select>
+      }
+      
+      <ul>
+        {coins.map((coin) => (
+          <li>
+            {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
